3c80fec873c7719bf25f9b36b45532ff5bb466d2
刘裕 <liuyu@healthmall.cn>
2017-10-19 15:05:30 +0800

    增加生产环境和调试环境启动命令，修改tool.js

--- utility/tool.js
+++ utility/tool.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+const fs = require('fs');
 
 /**
  * 搜索JSON数组
@@ -7,24 +7,24 @@ var fs = require('fs');
  * @returns {Object} 匹配的JSON对象
  */
 exports.jsonQuery = function (jsonArray, conditions) {
-    var i = 0,
-        len = jsonArray.length,
-        json,
-        condition,
-        flag;
-    for (; i < len; i++) {
-        flag = true;
-        json = jsonArray[i];
-        for (condition in conditions) {
-            if (json[condition] !== conditions[condition]) {
-                flag = false;
-                break;
-            }
-        }
-        if (flag) {
-            return json;
-        }
-    }
+	let i = 0,
+		len = jsonArray.length,
+		json,
+		condition,
+		flag;
+	for (; i < len; i++) {
+		flag = true;
+		json = jsonArray[i];
+		for (condition in conditions) {
+			if (json[condition] !== conditions[condition]) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag) {
+			return json;
+		}
+	}
 };
 
 /**
@@ -34,21 +34,21 @@ exports.jsonQuery = function (jsonArray, conditions) {
  * @param callback 回调函数
  */
 exports.getConfig = function (filePath, key, callback) {
-    if(typeof key === 'function'){
-        callback = key;
-        key = undefined;
-    }
-    fs.readFile(filePath, 'utf8', function (err, file) {
-        if (err) {
-            console.log('读取文件%s出错：' + err, filePath);
-            return callback(err);
-        }
-        var data = JSON.parse(file);
-        if (typeof key === 'string') {
-            data = data[key];
-        }
-        return callback(null, data);
-    });
+	if (typeof key === 'function') {
+		callback = key;
+		key = undefined;
+	}
+	fs.readFile(filePath, 'utf8', function (err, file) {
+		if (err) {
+			console.log(`读取文件${filePath}出错：${err}`);
+			return callback(err);
+		}
+		let data = JSON.parse(file);
+		if (typeof key === 'string') {
+			data = data[key];
+		}
+		return callback(null, data);
+	});
 };
 
 /**
@@ -57,15 +57,15 @@ exports.getConfig = function (filePath, key, callback) {
  * @param setters 要写入的对象
  */
 exports.setConfig = function (filePath, setters) {
-    fs.readFile(filePath, 'utf8', function (err, file) {
-        var data = JSON.parse(file),
-            key;
-        for (key in setters) {
-            data[key] = setters[key];
-        }
-        var newFile = JSON.stringify(data, null, 2);
-        fs.writeFile(filePath, newFile, 'utf8');
-    });
+	fs.readFile(filePath, 'utf8', function (err, file) {
+		let data = JSON.parse(file),
+			key;
+		for (key in setters) {
+			data[key] = setters[key];
+		}
+		let newFile = JSON.stringify(data, null, 2);
+		fs.writeFile(filePath, newFile, 'utf8');
+	});
 };
 
 /**
@@ -75,24 +75,24 @@ exports.setConfig = function (filePath, setters) {
  * @returns {string} 拼装的key，带前缀的形如：prefix_name_Tom_age_20，不带前缀的形如：name_Tom_age_20
  */
 exports.generateKey = function (prefix, obj) {
-    if (typeof prefix === 'object') {
-        obj = prefix;
-        prefix = undefined;
-    }
-    var attr,
-        value,
-        key = '';
-    for (attr in obj) {
-        value = obj[attr];
-        //形如： _name_Tom
-        key += '_' + attr.toString().toLowerCase() + '_' + value.toString()
-    }
-    if (prefix) {
-        //形如：prefix_name_Tom_age_20
-        key = prefix + key;
-    } else {
-        //形如：name_Tom_age_20
-        key = key.substr(1);
-    }
-    return key;
+	if (typeof prefix === 'object') {
+		obj = prefix;
+		prefix = undefined;
+	}
+	let attr,
+		value,
+		key = '';
+	for (attr in obj) {
+		value = obj[attr];
+		//形如： _name_Tom
+		key += '_' + attr.toString().toLowerCase() + '_' + value.toString()
+	}
+	if (prefix) {
+		//形如：prefix_name_Tom_age_20
+		key = prefix + key;
+	} else {
+		//形如：name_Tom_age_20
+		key = key.substr(1);
+	}
+	return key;
 };
